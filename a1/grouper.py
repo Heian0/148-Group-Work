e94a6a60f3de8ec993f389957577001bf191e299
Jeffery Khang <jeffreykhang2004@gmail.com>
2023-02-21 18:26:28 -0500

    test

--- a1/grouper.py
+++ a1/grouper.py
@@ -22,7 +22,9 @@ Jonathan Calver, and Jacqueline Smith
 This file contains classes that define different algorithms for grouping
 students according to chosen criteria and the group members' answers to survey
 questions. This file also contains a class that describes a group of students
-as well as one that describes a grouping (a collection of groups).
+as well as one that describes a grouping (a collection of groups).s
+
+#jeffery is gay
 """
 from __future__ import annotations
 
